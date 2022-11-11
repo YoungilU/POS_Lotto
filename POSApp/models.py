b07f1271816bc08d2debc9395ba3bef4cbe380c9
YOUNGIL_YU <sunforever58@gmail.com>
2022-11-11 21:25:28 +0900

    mainpage 함수 수정

--- POSApp/models.py
+++ POSApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class POSDB(models.Model):
+    seq = models.IntegerField(primary_key=True)
+    pension_lottery_1000 = models.IntegerField(null=True)
+    pension_lottery_5000 = models.IntegerField(null=True)
+    instant_lottery_1000 = models.IntegerField(null=True)
+    instant_lottery_2000 = models.IntegerField(null=True)
+    balance = models.IntegerField(null=True)
+    daily_sales = models.IntegerField(null=True)
--- POSApp/views.py
+++ POSApp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from POSApp.models import POSDB
+
+
 # Create your views here.
 def index(request):
+    print(POSDB.objects.all().values())
     return render(request, "index.html")
 
 def test(request):
